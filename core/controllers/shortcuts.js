651a9e564ffe4d22b311e75700816287feb1f5ff
Optimusprimeagario <optimusprimeagario@gmail.com>
2017-03-03 10:56:18 -0800

    Update shortcuts.js

--- core/controllers/shortcuts.js
+++ core/controllers/shortcuts.js
@@ -48,3 +48,15 @@ document.addEventListener("keydown", function(e) {
         default: break;
     };
 });
+
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+document.getElementById("he2").remove();
